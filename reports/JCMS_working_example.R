2d741feb6331744beddbc3e1400a8e356ece5e17
J.B <jessica.k.bonnie@gmail.com>
2015-08-25 13:01:47 -0400

    added example of working file

---
+++ reports/JCMS_working_example.R
@@ -0,0 +1,20 @@
+
+rcrds <- read.csv("data/JCMS.txt")
+
+# load all necessary packages
+library(dplyr)
+library(ggthemes)
+library(pander)
+
+JCMS <- filter(rcrds, YEAR_OF_FILING==2015)
+
+
+JCMSMonthly <- 
+  JCMS %>%
+  filter(CASE_TYPE_CD=="MC") %>%
+  group_by(MONTH_OF_FILING) %>%
+  summarise(count = n())
+
+  
+pander(JCMSMonthly, caption = "Monthly Frequency of Commitment Hearings for Minors, 2015")
+
