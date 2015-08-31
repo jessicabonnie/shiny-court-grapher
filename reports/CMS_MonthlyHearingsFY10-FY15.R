308db9cc80ac642bed720fb0babad1a9df046b81
JB <jessia.k.bonnie@gmail.com>
2015-08-30 21:08:13 -0400

    Temporary waypoint

--- reports/CMS_MonthlyHearingsFY10-FY15.R
+++ reports/CMS_MonthlyHearingsFY10-FY15.R
@@ -1,46 +1,17 @@
-CMS <- read.csv("data/CMS_8_26.txt")
+
+source("reports/CMS_prep.R")
+#CMS <- read.csv("data/CMS_8_26.txt")
 
 # load all necessary packages
 library(dplyr)
-library(ggthemes)
+library(tidyr)
 library(pander)
+
+library(ggthemes)
+
 library(stringr)
 library(lubridate)
 
-#Create Month, Year
-CMS$HEAR.DATE <- as.Date(CMS$HEAR.DATE, format = '%m/%d/%Y')
-CMS$Year<- year(CMS$HEAR.DATE)
-CMS$Month <- month(CMS$HEAR.DATE)
-
-#Filter for no errors (keep or no?)
-CMS<- filter(CMS, Error< 1|is.na(Error))
-
-# Create Fiscal Year Variable
-CMS$FYear<- CMS$Year
-CMS[CMS$Month > 6, ]$FYear <- as.numeric(CMS[CMS$Month > 6, ]$Year) + 1
-
-
-
-#Create Fiscal Quarter Variable
-CMS$FQtr <- CMS$Month
-CMS[CMS$Month == 7|CMS$Month==8| CMS$Month==9, ]$FQtr <- 1
-CMS[CMS$Month == 10|CMS$Month==11| CMS$Month==12, ]$FQtr <- 2
-CMS[CMS$Month == 1|CMS$Month==2| CMS$Month==3, ]$FQtr <- 3
-CMS[CMS$Month == 4|CMS$Month==5| CMS$Month==6, ]$FQtr <- 4
-
-# Create abbreviated month column, factored in accordance with fiscal calendar
-CMS$FYMonthAbbrev <- factor(substr(month.name[CMS$Month],1,3),levels=substr(c(month.name[7:12],month.name[1:6]),1,3))
-
-# Create a uniq identifier for the month (may or may not be needed)
-CMS$month_id <- factor(paste(CMS$FYear, str_pad(as.character(CMS$Month), 2, side="left", pad="0"), sep="-"))
-
-#Force FIPs into the correct format
-CMS$FIPS <- str_pad(as.character(CMS$FIPS),3,side="left",pad="0")
-
-
-#NOTE: ATTEMPT TO MAKE VARIABLE FOR RECOMMITMENT VS INITIAL HEARINGS. paycode 41 & 46 = recommitment. what is 21?
-CMS$initial <- ifelse (CMS$PAY.CD == 41 | CMS$PAY.CD == 46, TRUE, FALSE)
-CMS$initial [is.na(CMS$PAY.CD)] <- FALSE
 
 #creating a chart of monthly hearing counts by FY. Note, for this dataset I need to exclude july and august of FY09
 #need to also group by recommitment vs. initial.
@@ -60,6 +31,7 @@ CMS_MonthlyHearings2 <-
   summarise(count = n())%>%
   filter(FYear!="2009")
 
+pander(CMS_MonthlyHearings, caption = "Monthly Frequency of Commitment Hearings for Adults, FY10-FY15")
 p <- 
   ggplot(CMS_MonthlyHearings2, aes(factor(FYMonthAbbrev), count, group=factor(FYear), color=factor(FYear))) + 
   geom_line() +
