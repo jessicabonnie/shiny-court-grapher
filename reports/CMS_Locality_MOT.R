2b0897a77bb59ef58a712adba3269dc8d8ac1a77
Ashleigh Allen <ashleigh.allen@gmail.com>
2015-09-02 18:05:23 -0400

    Gorgeous MOT Table perfected.

--- reports/CMS_Locality_MOT.R
+++ reports/CMS_Locality_MOT.R
@@ -4,53 +4,55 @@ library(pander)
 library(dplyr)
 library(tidyr)
 
-CMS <- read.csv("../data/CMS_8_26.txt")
-FIPS_Codes <- read.csv("../data/FIPS_R.csv")
+# source ("reports/CMS_prep.R")
 
-# Remove Extraneous columns from CMS table
-CMS <- CMS[,!names(CMS) %in% c("Notes","Received","ErrorType","FilingDate","CorrectionDate")]
+#CMS <- read.csv("data/CMS_8_26.txt")
+#FIPS_Codes <- read.csv("/data/FIPS_R.csv")
 
-#Remove extra white space from around some values
-CMS$HEAR.RSLT <- str_trim(CMS$HEAR.RSLT)
-CMS$CASE.TYP <- str_trim(CMS$CASE.TYP)
-CMS$MOT <- str_trim(CMS$MOT)
-
-#Create Month and Year columns
-CMS$HEAR.DATE <- as.Date(CMS$HEAR.DATE, format = '%m/%d/%Y')
-CMS$Year<- year(CMS$HEAR.DATE)
-CMS$Month <- month(CMS$HEAR.DATE)
-
-#Filter for no errors (keep or no?)
-CMS<- filter(CMS, Error< 1|is.na(Error))
-
-# Create Fiscal Year Variable
-CMS$FYear<- CMS$Year
-CMS[CMS$Month > 6, ]$FYear <- as.numeric(CMS[CMS$Month > 6, ]$Year) + 1
-
-#Create Fiscal Quarter Variable
-CMS$FQtr <- CMS$Month
-CMS[CMS$Month == 7|CMS$Month==8| CMS$Month==9, ]$FQtr <- 1
-CMS[CMS$Month == 10|CMS$Month==11| CMS$Month==12, ]$FQtr <- 2
-CMS[CMS$Month == 1|CMS$Month==2| CMS$Month==3, ]$FQtr <- 3
-CMS[CMS$Month == 4|CMS$Month==5| CMS$Month==6, ]$FQtr <- 4
-
-# Create abbreviated month column, factored in accordance with fiscal calendar
-CMS$FYMonthAbbrev <- factor(substr(month.name[CMS$Month],1,3),levels=substr(c(month.name[7:12],month.name[1:6]),1,3))
-
-# Create a uniq identifier for the month (may or may not be needed)
-CMS$month_id <- factor(paste(CMS$FYear, str_pad(as.character(CMS$Month), 2, side="left", pad="0"), sep="-"))
-
-#Create FIPs column
-CMS$FIPS <- substr(CMS$CASE.NUMBER, 1, 4)
-
-#Include FIPS names
-CMS <- merge(CMS, FIPS_Codes, by = c("FIPS"), all.x = TRUE)
-CMS <- CMS[,!names(CMS) %in% c("SHORT_FIPS","COURT")]
-names(CMS)[names(CMS)=="NAME"] <- "Locality"
-
-#Use Pay Code to determine if Initial
-CMS$initial <- ifelse (CMS$PAY.CD == 41 | CMS$PAY.CD == 46, FALSE, TRUE)
-CMS$initial [is.na(CMS$PAY.CD)] <- TRUE
+# # Remove Extraneous columns from CMS table
+# CMS <- CMS[,!names(CMS) %in% c("Notes","Received","ErrorType","FilingDate","CorrectionDate")]
+# 
+# #Remove extra white space from around some values
+# CMS$HEAR.RSLT <- str_trim(CMS$HEAR.RSLT)
+# CMS$CASE.TYP <- str_trim(CMS$CASE.TYP)
+# CMS$MOT <- str_trim(CMS$MOT)
+# 
+# #Create Month and Year columns
+# CMS$HEAR.DATE <- as.Date(CMS$HEAR.DATE, format = '%m/%d/%Y')
+# CMS$Year<- year(CMS$HEAR.DATE)
+# CMS$Month <- month(CMS$HEAR.DATE)
+# 
+# #Filter for no errors (keep or no?)
+# CMS<- filter(CMS, Error< 1|is.na(Error))
+# 
+# # Create Fiscal Year Variable
+# CMS$FYear<- CMS$Year
+# CMS[CMS$Month > 6, ]$FYear <- as.numeric(CMS[CMS$Month > 6, ]$Year) + 1
+# 
+# #Create Fiscal Quarter Variable
+# CMS$FQtr <- CMS$Month
+# CMS[CMS$Month == 7|CMS$Month==8| CMS$Month==9, ]$FQtr <- 1
+# CMS[CMS$Month == 10|CMS$Month==11| CMS$Month==12, ]$FQtr <- 2
+# CMS[CMS$Month == 1|CMS$Month==2| CMS$Month==3, ]$FQtr <- 3
+# CMS[CMS$Month == 4|CMS$Month==5| CMS$Month==6, ]$FQtr <- 4
+# 
+# # Create abbreviated month column, factored in accordance with fiscal calendar
+# CMS$FYMonthAbbrev <- factor(substr(month.name[CMS$Month],1,3),levels=substr(c(month.name[7:12],month.name[1:6]),1,3))
+# 
+# # Create a uniq identifier for the month (may or may not be needed)
+# CMS$month_id <- factor(paste(CMS$FYear, str_pad(as.character(CMS$Month), 2, side="left", pad="0"), sep="-"))
+# 
+# #Create FIPs column
+# CMS$FIPS <- substr(CMS$CASE.NUMBER, 1, 4)
+# 
+# #Include FIPS names
+# CMS <- merge(CMS, FIPS_Codes, by = c("FIPS"), all.x = TRUE)
+# CMS <- CMS[,!names(CMS) %in% c("SHORT_FIPS","COURT")]
+# names(CMS)[names(CMS)=="NAME"] <- "Locality"
+# 
+# #Use Pay Code to determine if Initial
+# CMS$initial <- ifelse (CMS$PAY.CD == 41 | CMS$PAY.CD == 46, FALSE, TRUE)
+# CMS$initial [is.na(CMS$PAY.CD)] <- TRUE
 
 ########### HERE'S THE MEAT #########
 # Create table of the counts of 4 Different MOT Types for all localities
@@ -73,7 +75,9 @@ CMS_MOT[is.na(CMS_MOT)] <- 0
 
 CMS_MOT$Total <- CMS_MOT$TYPE1 + CMS_MOT$TYPE2 + CMS_MOT$TYPE3 + CMS_MOT$TYPE4
 
-names(CMS_MOT) <- c("Locality /n whatever", "TYPE /n 1", "TYPE2", "TYP3", "TYPE4", "Total")
+names(CMS_MOT) <- c("Locality", "Direct", "New\nHearing", "Discharge\nInitial", "Discharge\nRecommitment", "Total")
+CMS_MOT$Locality <- gsub("/","/\n",CMS_MOT$Locality)
+CMS_MOT$Locality <- gsub(" \\(","\n\\(",CMS_MOT$Locality)
 
 # Trying to give MOT type column names
 #CMS_MOT <- as.data.frame(CMS_MOT,colnames)
@@ -83,4 +87,6 @@ names(CMS_MOT) <- c("Locality /n whatever", "TYPE /n 1", "TYPE2", "TYP3", "TYPE4
 # 
 # CMS_MOT<- rename(CMS_MOT, Stepdown_Recommitment_Discharge=V1, Stepdown_Initial_Discharge=V2, Stepdown_New_Hearing=V3, Direct=V4)
 # CMS_MOT <- CMS_MOT[-50,]
-pander(CMS_MOT, caption= "MOT Types by Locality")
+
+
+#pander(CMS_MOT, caption= "MOT Types by Locality", keep.line.breaks = TRUE,split.table = Inf))
