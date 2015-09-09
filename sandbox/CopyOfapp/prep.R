7ab46ac250731902924017a0bfd30ce0cb2c8bfb
realviacauchy <jessia.k.bonnie@gmail.com>
2015-09-09 18:12:11 -0400

    beginning app experimentation

---
+++ sandbox/CopyOfapp/prep.R
@@ -0,0 +1,26 @@
+# helper script for the ilppp app
+
+# load all packages
+library(rgdal)
+library(mapproj)
+library(ggplot2)
+library(dplyr)
+library(maptools)
+library(ggthemes)
+library(gpclib)
+library(ggmap)
+
+# fix for gpc error
+gpclibPermit()
+
+# read in shp files
+# note the file structure may change but you have to use the '.' syntax to get everything
+csb <- readOGR(dsn = "../../map/.", layer = "CSBfinal_1")
+
+# create id variable
+csb@data$id <- rownames(csb@data)
+
+# create dataframe so ggplot can do its thing
+csb.points <- fortify(csb, region="id")
+csb.df <- right_join(csb.points, csb@data, by="id")
+
