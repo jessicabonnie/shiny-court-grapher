689d8102f33b9336f02c12aa864422d1029ce90d
Ashleigh Allen <ashleigh.allen@gmail.com>
2015-09-16 15:32:22 -0400

    Adding FID to MC_Locality table

--- reports/CMS_MCPercent_Locality_FY10-FY15_TABLE.R
+++ reports/CMS_MCPercent_Locality_FY10-FY15_TABLE.R
@@ -12,9 +12,9 @@ thing <- select(thing,FYear,CSBName,OBJECTID,FID,HEAR.RSLT, CASE.TYP)
 
 CMS_Invol <- 
   filter(thing, CASE.TYP =="MC", FYear > 2009) %>%
-  group_by(CSBName,FYear, HEAR.RSLT)%>%
+  group_by(CSBName,FID, FYear, HEAR.RSLT)%>%
   tally %>%
-  group_by(CSBName,FYear) %>%
+  group_by(CSBName, FID, FYear) %>%
   mutate(pct=(100*n)/sum(n)) %>%
   select(-n)%>%
   spread(HEAR.RSLT,pct,fill = 0)
