9e425702e2008ef8b8540f027da57c33f0701341
realviacauchy <jessia.k.bonnie@gmail.com>
2015-09-27 04:12:21 -0400

    It's messy, but I fixed the percentage problem. Like, a lot.

--- app/prep.R
+++ app/prep.R
@@ -34,19 +34,40 @@ CMS_Invol_pre$FID <- as.numeric(CMS_Invol_pre$FID)
 
 CMS_Invol <- 
   filter(CMS_Invol_pre, CASE.TYP =="MC", FYear > 2009) %>%
-  group_by(CSBName, FID, FYear, HEAR.RSLT)%>%
+  group_by(CSBName, FID, FYear, HEAR.RSLT) %>%
   tally %>%
   group_by(CSBName, FID, FYear) %>%
   mutate(pct=(100*n)/sum(n)) %>%
   select(-n)%>%
   spread(HEAR.RSLT,pct,fill = 0)
 
+cmsinvol<-
+  filter(CMS_Invol_pre, CASE.TYP =="MC", FYear > 2009) %>%
+  group_by(CSBName, FID, FYear, HEAR.RSLT) %>%
+  tally
+
+scaff <- expand.grid(CSBName=unique(cmsinvol$CSBName),
+                     FYear=unique(cmsinvol$FYear),
+                     HEAR.RSLT=unique(cmsinvol$HEAR.RSLT))
+
+scaff <- merge(x=scaff, y=CSB_FID[,c("CSBName","FID")], by=c("CSBName"),all.x=TRUE,all.Y=F)
+scaff$FID <-as.numeric(scaff$FID)
+cmsinvol <- left_join(x=scaff, y=cmsinvol, by=c("CSBName","FID","FYear","HEAR.RSLT"))
+cmsinvol[is.na(cmsinvol)]<-0
+
+cmsinvol <- cmsinvol%>%
+  group_by(CSBName, FID, FYear) %>%
+  mutate(pct=(100*n)/sum(n)) %>%
+  select(-n) %>%
+  spread(HEAR.RSLT,pct,fill = 0)
+
+
 csb_scaffold <- expand.grid(CSBName=unique(CMS_Invol$CSBName), FYear=unique(CMS_Invol$FYear)) 
 
 csb_scaffold <- left_join(x=csb_scaffold, y= CSB_FID[ , c("CSBName", "FID")], by=c("CSBName"))
 CMS_Invol<- left_join(x=csb_scaffold, y=CMS_Invol, by=c("CSBName", "FYear","FID"))
 
-CMS_Invol[is.na(CMS_Invol)] <- 50
+#CMS_Invol[is.na(CMS_Invol)] <- 50
 
 # read in shp files
 # note the file structure may change but you have to use the '.' syntax to get everything
@@ -61,7 +82,7 @@ csb_intake@data$id <- as.numeric(rownames(csb_intake@data))
 
 map_plot <- function (fy, dispos){
   csb <- csb_intake
-  sub_cms_invol <- filter(CMS_Invol, FYear == fy)
+  sub_cms_invol <- filter(cmsinvol, FYear == fy)
   csb@data <- merge(csb@data,sub_cms_invol,
                     all.x=T,by.x=c("id"),by.y=c("FID"))
   csb@data <- select(csb@data, -CSB_Name, -Percent_Ve,
